b109b8e9138228f71a3656c7068425927bf32ed5
jpinn97 <JP878@canterbury.ac.uk>
2023-04-24 00:27:56 +0100

    Fix for if user exists, is registered, not on CRM.

--- wp-content/themes/twentytwentythree-child/login-request.php
+++ wp-content/themes/twentytwentythree-child/login-request.php
@@ -4,75 +4,88 @@ require_once(dirname(__FILE__) . '/../../../wp-load.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['user_email-6'];
     $password = $_POST['user_password-6'];
-    try {
-        // SOAP request parameters
-        $url = 'https://discoverypark.evolutive.co.uk/services/wsForm.asmx';
-        $soapAction = 'http://tempuri.org/LoginForm';
-        $passcode = EVO_API_KEY;
 
-        // SOAP request body
-        $requestBody = <<<XML
-        <?xml version="1.0" encoding="utf-8"?>
-        <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-            <soap12:Body>
-                <LoginForm xmlns="http://tempuri.org/">
-                    <sPasscode>{$passcode}</sPasscode>
-                    <sEmailAddress>{$email}</sEmailAddress>
-                    <sPassword>{$password}</sPassword>
-                </LoginForm>
-            </soap12:Body>
-        </soap12:Envelope>
-        XML;
+    // Get the user object based on the email
+    $user = get_user_by('email', $email);
 
-        // Set SOAP headers
-        $headers = array(
-            'Content-Type: text/xml; charset=utf-8',
-            'SOAPAction: ' . $soapAction
-        );
-
-        // Initialize cURL session
-        $ch = curl_init();
-
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        // Execute the cURL request
-        $response = curl_exec($ch);
-
-        // Log the SOAP response
-        error_log("SOAP Response: " . $response);
-
-        // Check for errors
-        if (curl_errno($ch)) {
-            throw new Exception('Error: ' . curl_error($ch));
+    // If the user exists in the WordPress database, set the auth cookie and log them in
+    if ($user) {
+        if (wp_check_password($password, $user->data->user_pass, $user->ID)) {
+            wp_set_auth_cookie($user->ID, true);
+            wp_set_current_user($user->ID, $user->user_login);
+            wp_redirect(home_url('/user/'));
+            exit();
+        } else {
+            // Redirect to the registration page
+            wp_redirect(home_url('/register/'));
+            exit();
         }
+    } else {
+        // The user does not exist in the WordPress database, proceed with the SOAP request
+        try {
+            // SOAP request parameters
+            $url = 'https://discoverypark.evolutive.co.uk/services/wsForm.asmx';
+            $soapAction = 'http://tempuri.org/LoginForm';
+            $passcode = EVO_API_KEY;
 
-        // Close cURL session
-        curl_close($ch);
+            // SOAP request body
+            $requestBody = <<<XML
+            <?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <LoginForm xmlns="http://tempuri.org/">
+                        <sPasscode>{$passcode}</sPasscode>
+                        <sEmailAddress>{$email}</sEmailAddress>
+                        <sPassword>{$password}</sPassword>
+                    </LoginForm>
+                </soap12:Body>
+            </soap12:Envelope>
+            XML;
 
-        // Load the SOAP response into a SimpleXML object
-        $xml = simplexml_load_string($response);
+            // Set SOAP headers
+            $headers = array(
+                'Content-Type: text/xml; charset=utf-8',
+                'SOAPAction: ' . $soapAction
+            );
 
-        $profile_fields = array("sFirstname", "sSurname", "sEmail", "sPassword", "sUsername", "sTelephoneNo");
-        $field_values = array();
+            // Initialize cURL session
+            $ch = curl_init();
 
-        foreach ($profile_fields as $field) {
-            $value = (string) $xml->xpath("//{$field}")[0] ?? null;
-            $field_values[$field] = $value;
-        }
+            // Set cURL options
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-        // Check if the <sEmail> and <sPassword> elements are present within the <User> element and if their values match the provided $email and $password
-        if (strtolower($field_values['sEmail']) === strtolower($email) && $field_values['sPassword'] === $password) {
+            // Execute the cURL request
+            $response = curl_exec($ch);
 
-            // Get the user object based on the email
-            $user = get_user_by('email', $email);
+            // Log the SOAP response
+            error_log("SOAP Response: " . $response);
 
-            // If the user does not exist, create a new user in the WordPress database
-            if (!$user) {
+            // Check for errors
+            if (curl_errno($ch)) {
+                throw new Exception('Error: ' . curl_error($ch));
+            }
+
+            // Close cURL session
+            curl_close($ch);
+
+            // Load the SOAP response into a SimpleXML object
+            $xml = simplexml_load_string($response);
+
+            $profile_fields = array("sFirstname", "sSurname", "sEmail", "sPassword", "sUsername", "sTelephoneNo");
+            $field_values = array();
+
+            foreach ($profile_fields as $field) {
+                $value = (string) $xml->xpath("//{$field}")[0] ?? null;
+                $field_values[$field] = $value;
+            }
+
+            // Check if the <sEmail> and <sPassword> elements are present within the <User> element and if their values match the provided $email and $password
+            if (strtolower($field_values['sEmail']) === strtolower($email) && $field_values['sPassword'] === $password) {
+
                 // Use a random username or generate one based on the email address
                 $username = 'user_' . wp_generate_password(6, false);
 
@@ -87,25 +100,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Set the user role
                 $user = new WP_User($user_id);
                 $user->set_role('um_tenant'); // Set the user role as needed
-            }
-            // If the user exists, set the auth cookie and log them in
-            if ($user) {
+
+                // Set the auth cookie and log the user in
                 wp_set_auth_cookie($user->ID, true);
                 wp_set_current_user($user->ID, $user->user_login);
                 wp_redirect(home_url('/user/'));
                 exit();
+            } else {
+                // Redirect to the registration page
+                wp_redirect(home_url('/register/'));
+                exit();
             }
-        } else {
-            // Redirect to the registration page
-            wp_redirect(home_url('/register/'));
-            exit();
-        }
-    } catch (Exception $e) {
-        // Login failed (SOAP request failed or user creation failed)
-        $error_message = $e->getMessage();
-        echo $error_message;
+        } catch (Exception $e) {
+            // Login failed (SOAP request failed or user creation failed)
+            $error_message = $e->getMessage();
+            echo $error_message;
 
-        // Log the error message
-        error_log("Error: " . $error_message);
+            // Log the error message
+            error_log("Error: " . $error_message);
+        }
     }
 }
