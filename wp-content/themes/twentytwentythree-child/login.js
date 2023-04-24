debc0381f49cbe4603c207dade8bcb419af25118
jpinn97 <JP878@canterbury.ac.uk>
2023-04-24 16:14:21 +0100

    jQuery for login.hs priority over POST

--- wp-content/themes/twentytwentythree-child/login.js
+++ wp-content/themes/twentytwentythree-child/login.js
@@ -1,10 +1,20 @@
-const loginForm = document.getElementById("um_form_6");
-const emailInput = document.getElementById("user_email");
-const passwordInput = document.getElementById("user_password");
+jQuery(document).ready(function ($) {
+  const loginForm = $(".um-form");
+  const emailInput = $("#user_email-6");
+  const passwordInput = $("#user_password-6");
+  const loginButton = $("#um-submit-btn"); // Add a selector for the login button
 
-loginForm.addEventListener("um-submit-btn", function(event) {
-  if (!emailInput.value || !passwordInput.value) {
-    event.preventDefault(); // cancel the form submission
-    alert("Please fill in all required fields.");
+  if (loginForm.length) {
+    loginForm.unbind("submit").bind("submit", function (event) {
+      alert("submit event fired");
+
+      if (!emailInput.val() || !passwordInput.val()) {
+        event.preventDefault(); // cancel the form submission
+        alert("Please fill in all required fields.");
+        loginButton.removeAttr("disabled"); // Re-enable the login button
+      }
+    });
+  } else {
+    alert("loginForm not found");
   }
 });
